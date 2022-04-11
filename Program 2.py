0ab57d41d7712c82e947c6f4c2ab254070c2927a
joannamickamedina <90305331+joannamickamedina@users.noreply.github.com>
2022-04-11 11:20:19 +0800

    Add files via upload

---
+++ Program 2.py
@@ -0,0 +1,19 @@
+from tkinter import *
+window=Tk()
+
+label = Label(window, text='Enter your fullname: ', fg='red')
+label.place(x=20, y=50)
+
+txtfld = Entry(window, text="This is a text field", bd=5)
+txtfld.place(x=200, y=50)
+
+button = Button(window, text='Click to display you Fullname', fg='red')
+button.place(x=20, y=100)
+
+txtfld = Entry(window, bd=5)
+txtfld.place(x=200, y=100)
+
+
+window.title('Midterm in OOP')
+window.geometry("400x200+10+10")
+window.mainloop()
