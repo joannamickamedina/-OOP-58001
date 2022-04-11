4362405af990a73826cd34233dc63980338f0c3f
joannamickamedina <90305331+joannamickamedina@users.noreply.github.com>
2022-04-11 11:25:58 +0800

    Update Program 2.py

--- Program 2.py
+++ Program 2.py
@@ -10,7 +10,7 @@ txtfld.place(x=200, y=50)
 button = Button(window, text='Click to display you Fullname', fg='red')
 button.place(x=20, y=100)
 
-txtfld = Entry(window, bd=5)
+txtfld = Entry(window, text="This is a text field", bd=5)
 txtfld.place(x=200, y=100)
 
 
